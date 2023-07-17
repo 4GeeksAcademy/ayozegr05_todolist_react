2abdeff255c8dd77e25c4b6e1a4b7a3ea96f8ceb
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-07-17 18:17:33 +0000

    Finished. Bugs fixed

--- src/js/component/TaskList.jsx
+++ src/js/component/TaskList.jsx
@@ -4,16 +4,20 @@ import React, { useState } from "react";
 const TaskList = () => {
   const [tasks, setTasks] = useState(["Read", "Eat", "Drink"]);
   const [newTask, setNewTask] = useState("");
+  const [count, setCount] = useState(3);
 
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
-    event.preventDefault(); 
-
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, newTask]);
+    event.preventDefault();
+  
+    if (newTask !== "") {
+      if (!tasks.includes(newTask)) {
+        setTasks([...tasks, newTask]);
+        setCount(count + 1);
+      }
       setNewTask("");
     }
   };
@@ -21,10 +25,12 @@ const TaskList = () => {
   const handleRemoveTask = (taskToRemove) => {
     const updatedTasks = tasks.filter((task) => task !== taskToRemove);
     setTasks(updatedTasks);
+    setCount(count - 1);
+
   };
 
   return (
-    <div className="container">
+    <div className="container  mx-0 px-0">
     <form className="mt-4" onSubmit={handleFormSubmit}>
       <input
       className="mb-4 p-2"
@@ -43,6 +49,7 @@ const TaskList = () => {
         ))}
       </ul>
       </div>
+      <p className="d-flex text-secondary border-top mt-4 mb-0 ps-3 pt-2 pb-0">{count} item left</p>
     </form>
     </div>
   );
